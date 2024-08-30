8386d698868946e5f7d7082d3e0018350ede6914
John Siordia <siohn@proton.me>
2024-08-29 21:41:43 -0500

    ModalWithForm refactor

--- se_project_react/src/components/App/App.jsx
+++ se_project_react/src/components/App/App.jsx
@@ -16,7 +16,38 @@ function App() {
         <Main weatherData={weatherData} />
         <Footer />
       </div>
-      <ModalWithForm />
+      <ModalWithForm title="New Garment" buttonText="Add garment">
+        <label htmlFor="name" className="modal__label">
+          Name{" "}
+        </label>
+        <input
+          type="text"
+          className="modal__input"
+          id="name"
+          placeholder="Name"
+        />
+        <label htmlFor="imageUrl" className="modal__label">
+          Image{" "}
+        </label>
+        <input
+          type="url"
+          className="modal__input"
+          id="imageUrl"
+          placeholder="Image URL"
+        />
+        <fieldset className="modal__radio-buttons">
+          <legend className="modal__legend">Select the weather type:</legend>
+          <label htmlFor="hot" className="modal__label_type_radio">
+            <input type="radio" className="modal__radio-input" id="hot" /> Hot
+          </label>
+          <label htmlFor="warm" className="modal__label_type_radio">
+            <input type="radio" className="modal__radio-input" id="warm" /> Warm
+          </label>
+          <label htmlFor="cold" className="modal__label_type_radio">
+            <input type="radio" className="modal__radio-input" id="cold" /> Cold
+          </label>
+        </fieldset>
+      </ModalWithForm>
     </div>
   );
 }
