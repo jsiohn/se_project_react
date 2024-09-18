b12feb784aecc9f90ba3cfd9b671af244fff1d7d
John Siordia <siohn@proton.me>
2024-09-18 15:46:10 -0500

    begin api request implementation

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,31 @@
 const baseUrl = "http://localhost:3001";
 
-function getItems() {
-  return fetch(`${baseUrl}/items`).then((res) => {
-    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
-  });
+function checkResponse(res) {
+  return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
 }
 
-export { getItems };
+function getItems() {
+  return fetch(`${baseUrl}/items`, {
+    method: "GET",
+  }).then(checkResponse);
+}
+
+function addItem() {
+  return fetch(`${baseUrl}/items`, {
+    method: "POST",
+    headers: "Content-type: application/json",
+    body: JSON.stringify({
+      name: name,
+      imageUrl: imageUrl,
+      weather: weather,
+    }),
+  }).then(checkResponse);
+}
+
+function deleteItem() {
+  return fetch(`${baseUrl}/items`, {
+    method: "DELETE",
+  }).then(checkResponse);
+}
+
+export { getItems, addItem, deleteItem };
